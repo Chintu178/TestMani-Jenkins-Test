e4ce7f48d8c2be61a8342da7d6c4642a61eecca1
chintu <chintu@172.20.10.2>
2020-03-23 14:57:13 -0400

    if code

---
+++ TestProject/src/TestIfConditionCheck.java
@@ -0,0 +1,27 @@
+
+public class TestIfConditionCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World this the Result");
+		{
+			int x = 10;
+			int y = 10;
+			
+			if(x > y){
+				System.out.println("X is grater then Y");
+			}
+			if(x == y){
+				System.out.println("X is equal to Y");
+			}
+			if(x < y){
+				System.out.println("X is less then Y");
+			}
+		}
+		
+	}
+
+	}
+
+
