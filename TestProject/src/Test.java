4f474f1d3ead3ed37619fede02cdfd8254883616
chintu <chintu@172.20.10.2>
2020-03-23 10:53:36 -0400

    add code is added

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -2,5 +2,15 @@
 public class Test {
 	public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        
+        
+        int x = 10;
+        int y = 15;
+        int result = x + y;
+        
+        System.out.println(result);
+        
+        
+    
 	}
 }
