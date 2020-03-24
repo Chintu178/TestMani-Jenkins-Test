7869840993e0ab91cdea455999ffc77e963937f1
chintu <chintu@172.20.10.2>
2020-03-24 13:16:19 -0400

    whileLoop

---
+++ TestProject/src/WhileLoop.java
@@ -0,0 +1,16 @@
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int x = -10; 
+    
+    while (x <= 0){
+    	System.out.println("x = " + x);
+    	x++;
+    	//x = x +1; similar condition
+    	//x += 1;
+    }
+	}
+
+}
