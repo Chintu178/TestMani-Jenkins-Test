ac8e5057792b7ac7524f093a04edf39748afec27
chintu <chintu@172.20.10.2>
2020-03-24 12:49:41 -0400

    forloop code

---
+++ TestProject/src/TestForLoop.java
@@ -0,0 +1,15 @@
+
+public class TestForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i = 0; i <= 10; i++) {
+			System.out.println("i = " + i);
+			
+		}for (int y = 10; y > 0; y--) {
+			System.out.println("y = " + y);
+		}
+	}
+	
+}
