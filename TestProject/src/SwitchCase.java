e71e16d45205712fa0cf70c6b2216fbcb41f8ddf
chintu <chintu@172.20.10.2>
2020-03-24 13:54:43 -0400

    Switch Case

---
+++ TestProject/src/SwitchCase.java
@@ -0,0 +1,29 @@
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 3;
+		
+		switch(j) {
+		
+			case 0:
+			System.out.println("Value is 0");
+			break;
+			
+			case 1:
+			System.out.println("Value is 1");
+			break;
+			
+			case 2:
+			System.out.println("Value is 2");
+			break;
+			
+			case 3:
+				System.out.println("Value is 3");
+				break;
+		}
+
+	}
+
+}
