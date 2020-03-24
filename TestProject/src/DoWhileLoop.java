40c7b2be97fa690d8ac8c83ab47f338dca60217e
chintu <chintu@172.20.10.2>
2020-03-24 13:35:59 -0400

    dowhileloop code

---
+++ TestProject/src/DoWhileLoop.java
@@ -0,0 +1,19 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 10;
+		
+		do {
+			System.out.println("x = " + x);
+			x--;
+		}while(x > 0);
+		// D/f b/w whileLoop & doWhileLoop
+		//WhileLoop print statement after checking the condition.
+		//DowhileLoop executes code once, print statement & then checks for the condition.
+		
+		}
+
+}
